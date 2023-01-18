7bd633b27ae389a4e8e5f00fa8551502d30100be
ToshihisaNagai <95553994+ToshihisaNagai@users.noreply.github.com>
2023-01-18 11:06:11 +0900

    laravel-todo-appをアップロード

---
+++ GoalController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreGoalRequest;
+use App\Http\Requests\UpdateGoalRequest;
+use App\Models\Goal;
+use Illuminate\Support\Facades\Auth;
+
+class GoalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $goals = Auth::user()->goals;
+
+        return view('goals.index', compact('goals'));
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreGoalRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreGoalRequest $request)
+    {
+        $request->validate([
+            'title' => 'required',
+        ]);
+
+        $goal = new Goal();
+        $goal->title = $request->input('title');
+        $goal->user_id = Auth::id();
+        $goal->save();
+
+        return redirect()->route('goals.index');  
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateGoalRequest  $request
+     * @param  \App\Models\Goal  $goal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateGoalRequest $request, Goal $goal)
+    {
+        $request->validate([
+            'title' => 'required',
+        ]);
+
+        $goal->title = $request->input('title');
+        $goal->user_id = Auth::id();
+        $goal->save();
+
+        return redirect()->route('goals.index'); 
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Goal  $goal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Goal $goal)
+    {
+        $goal->delete();
+ 
+        return redirect()->route('goals.index');
+    }
+}
